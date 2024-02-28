dd9f25efa984f606ab5b07cc3869395e3f44fb2c
John Doe <kmusab9@gmail.com>
2024-02-28 15:28:22 +0400

    that's my today's update

--- database/migrations/2024_02_28_091943_create_pos_orders_table.php
+++ database/migrations/2024_02_28_091943_create_pos_orders_table.php
@@ -11,24 +11,21 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('pos__orders', function (Blueprint $table) {
+        Schema::create('pos_orders', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('product_id')->references('id')->on('products')->onDelete('cascade');
             $table->foreignId('customer_id')->references('id')->on('customers')->onDelete('cascade');
             $table->foreignId('store_id')->references('id')->on('stores')->onDelete('cascade');
             $table->integer('item_count');
             $table->decimal('paid_amount',50,3);
             $table->decimal('total_amount',50,3);
-            $table->string('discount_type')->nullable;
+            $table->integer('discount_type')->nullable;
+            $table->integer('discount_by')->nullable;
             $table->decimal('total_tax',50,3);
             $table->decimal('total_discount',50,3);
             $table->decimal('cash_back' ,50,3);
-            $table->string('payment_note')->nullable();
-            $table->string('payment_method');
+            $table->integer('return_status')->default(1);
             $table->text('notes')->nullable();
-            $table->date('add_date')->nullable();
             $table->string('added_by')->nullable();
-            $table->string('updated_by')->nullable();
             $table->string('user_id', 255)->nullable();
             $table->timestamps();
 
