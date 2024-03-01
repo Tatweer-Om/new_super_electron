519da8890ca39588ace24d585c5fba09b6d58e94
ahmed <ahmed@tatweer.om>
2024-03-01 16:30:36 +0400

    language with sidebar work

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -176,8 +176,8 @@
 						</a>
 						<div class="dropdown-menu notifications">
 							<div class="topnav-dropdown-header">
-								<span class="notification-title">Notifications</span>
-								<a href="javascript:void(0)" class="clear-noti"> Clear All </a>
+								<span class="notification-title">الإشعارات</span>
+								<a href="javascript:void(0)" class="clear-noti">  عرض الكل </a>
 							</div>
 							<div class="noti-content">
 								<ul class="notification-list">
@@ -188,7 +188,7 @@
 													<img alt="" src="{{ asset('img/profiles/avatar-02.jpg')}}">
 												</span>
 												<div class="media-body flex-grow-1">
-													<p class="noti-details"><span class="noti-title">John Doe</span> added new task <span class="noti-title">Patient appointment booking</span></p>
+													<p class="noti-details"><span class="noti-title">sسلطان</span> added new task <span class="noti-title"></span></p>
 													<p class="noti-time"><span class="notification-time">4 mins ago</span></p>
 												</div>
 											</div>
@@ -201,7 +201,7 @@
 													<img alt="" src="{{ asset('img/profiles/avatar-03.jpg')}}">
 												</span>
 												<div class="media-body flex-grow-1">
-													<p class="noti-details"><span class="noti-title">Tarah Shropshire</span> changed the task name <span class="noti-title">Appointment booking with payment gateway</span></p>
+													<p class="noti-details"><span class="noti-title">Tarah Shropshire</span> changed the task name <span class="noti-title"></span></p>
 													<p class="noti-time"><span class="notification-time">6 mins ago</span></p>
 												</div>
 											</div>
@@ -214,7 +214,7 @@
 													<img alt="" src="{{ asset('img/profiles/avatar-06.jpg')}}">
 												</span>
 												<div class="media-body flex-grow-1">
-													<p class="noti-details"><span class="noti-title">Misty Tison</span> added <span class="noti-title">Domenic Houston</span> and <span class="noti-title">Claire Mapes</span> to project <span class="noti-title">Doctor available module</span></p>
+													<p class="noti-details"><span class="noti-title">Misty Tison</span> added <span class="noti-title">Domenic Houston</span> and <span class="noti-title">Claire Mapes</span> to project <span class="noti-title"></span></p>
 													<p class="noti-time"><span class="notification-time">8 mins ago</span></p>
 												</div>
 											</div>
@@ -227,7 +227,7 @@
 													<img alt="" src="{{ asset('img/profiles/avatar-17.jpg')}}">
 												</span>
 												<div class="media-body flex-grow-1">
-													<p class="noti-details"><span class="noti-title">Rolland Webber</span> completed task <span class="noti-title">Patient and Doctor video conferencing</span></p>
+													<p class="noti-details"><span class="noti-title">Rolland Webber</span> completed task <span class="noti-title"></span></p>
 													<p class="noti-time"><span class="notification-time">12 mins ago</span></p>
 												</div>
 											</div>
@@ -240,7 +240,7 @@
 													<img alt="" src="{{ asset('img/profiles/avatar-13.jpg')}}">
 												</span>
 												<div class="media-body flex-grow-1">
-													<p class="noti-details"><span class="noti-title">Bernardo Galaviz</span> added new task <span class="noti-title">Private chat module</span></p>
+													<p class="noti-details"><span class="noti-title">Bernardo Galaviz</span> added new task <span class="noti-title"></span></p>
 													<p class="noti-time"><span class="notification-time">2 days ago</span></p>
 												</div>
 											</div>
@@ -276,15 +276,15 @@
 									<span class="user-img"><img src="{{ asset('img/profiles/avator1.jpg')}}" alt="">
 									<span class="status online"></span></span>
 									<div class="profilesets">
-										<h6>John Smilga</h6>
-										<h5>Super Admin</h5>
+										<h6>سلطان</h6>
+										<h5>مدير النظام</h5>
 									</div>
 								</div>
 								<hr class="m-0">
-								<a class="dropdown-item" href="profile.html"> <i class="me-2"  data-feather="user"></i> My Profile</a>
-								<a class="dropdown-item" href="generalsettings.html"><i class="me-2" data-feather="settings"></i>Settings</a>
+								<!-- <a class="dropdown-item" href="profile.html"> <i class="me-2"  data-feather="user"></i> My Profile</a> -->
+								<a class="dropdown-item" href="#"><i class="me-2" data-feather="settings"></i>الإعدادات</a>
 								<hr class="m-0">
-								<a class="dropdown-item logout pb-0" href="signin.html"><img src="{{ asset('img/icons/log-out.svg')}}" class="me-2" alt="img">Logout</a>
+								<a class="dropdown-item logout pb-0" href="signin.html"><img src="{{ asset('img/icons/log-out.svg')}}" class="me-2" alt="img">خروج</a>
 							</div>
 						</div>
 					</li>
@@ -295,9 +295,9 @@
 				<div class="dropdown mobile-user-menu">
 					<a href="javascript:void(0);" class="nav-link dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false"><i class="fa fa-ellipsis-v"></i></a>
 					<div class="dropdown-menu dropdown-menu-right">
-						<a class="dropdown-item" href="profile.html">My Profile</a>
-						<a class="dropdown-item" href="generalsettings.html">Settings</a>
-						<a class="dropdown-item" href="signin.html">Logout</a>
+						<!-- <a class="dropdown-item" href="#">My Profile</a> -->
+						<!-- <a class="dropdown-item" href="generalsettings.html">Settings</a> -->
+						<a class="dropdown-item" href="#">خروج</a>
 					</div>
 				</div>
 				<!-- /Mobile Menu -->
@@ -323,15 +323,34 @@
 										<li><a href="{{ url('brand') }}"><i data-feather="tag"></i><span>{{ trans('messages.brand_lang', [], session('locale')) }}</span></a></li>
 										<li><a href="{{ url('supplier') }}"><i data-feather="speaker"></i><span>{{ trans('messages.supplier_lang', [], session('locale')) }}</span></a></li>
 										<li><a href="{{ url('addproduct') }}"><i data-feather="plus-square"></i><span>{{ trans('messages.sidebar_add_stock_lang', [], session('locale')) }}</span></a></li>
-                                		<li><a href="{{  url('purchases')}}"><i data-feather="shopping-bag"></i><span>{{ trans('messages.add_purchase_lang', [], session('locale')) }}</span></a></li>
                                 		<li><a href="{{  url('products')}}"><i data-feather="database"></i><span>{{ trans('messages.view_stock_lang', [], session('locale')) }}</span></a></li>
                                 		<li><a href="{{  route('qty_audit')}}"><i data-feather="book"></i><span>{{ trans('messages.view_qty_audit_lang', [], session('locale')) }}</span></a></li>
-                                        <li><a href="{{  url('customer')}}"><i data-feather="award"></i><span>{{ trans('messages.customer_list_lang', [], session('locale')) }}</span></a></li>
                                         {{-- <li><a href="{{  url('pos')}}"><i data-feather="truck"></i><span>{{ trans('messages.pos_lang', [], session('locale')) }}</span></a></li> --}}
-                                        <li><a href="{{  url('workplace')}}"><i data-feather="briefcase"></i><span>{{ trans('messages.workplace_lang', [], session('locale')) }}</span></a></li>
-                                        <li><a href="{{  url('university')}}"><i data-feather="airplay"></i><span>{{ trans('messages.university_lang', [], session('locale')) }}</span></a></li>
+                                    
 									</li>
+
+									
 								</ul>
+
+								<li class="submenu">
+										<a href="javascript:void(0);"><i data-feather="smartphone"></i><span>{{ trans('messages.sidebar_customer', [], session('locale')) }}</span><span class="menu-arrow"></span></a>
+										<ul>
+
+                                        <li><a href="{{  url('customer')}}"><i data-feather="award"></i><span>{{ trans('messages.customer_list_lang', [], session('locale')) }}</span></a></li>
+										<li><a href="{{  url('workplace')}}"><i data-feather="briefcase"></i><span>{{ trans('messages.workplace_lang', [], session('locale')) }}</span></a></li>
+                                        <li><a href="{{  url('university')}}"><i data-feather="airplay"></i><span>{{ trans('messages.university_lang', [], session('locale')) }}</span></a></li>
+
+									</li>
+							</li>
+						</ul>
+
+						<li class="submenu">
+										<a href="javascript:void(0);"><i data-feather="smartphone"></i><span>{{ trans('messages.sidebar_accounting', [], session('locale')) }}</span><span class="menu-arrow"></span></a>
+										<ul>
+                                		<li><a href="{{  url('account')}}"><i data-feather="shopping-bag"></i><span>{{ trans('messages.sidebar_bank_lang', [], session('locale')) }}</span></a></li>
+                                		<li><a href="{{  url('purchases')}}"><i data-feather="shopping-bag"></i><span>{{ trans('messages.add_purchase_lang', [], session('locale')) }}</span></a></li>
+
+									</li>
 							</li>
 						</ul>
 					</div>
